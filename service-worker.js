74e34cf9d54f38b4154477882f14ed9375c0f494
sanjeevjha24oct <2j66z929fn@privaterelay.appleid.com>
2026-08-04 01:22:43 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='boost-quote-v2';
+const CACHE='boost-sales-pro-v1';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
